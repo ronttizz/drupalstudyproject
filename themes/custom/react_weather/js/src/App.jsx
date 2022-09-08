83c421fe80c6734464b2d39d1c05ebf496f6ca31
ronttizz <toni.nissinen@gmail.com>
2022-09-08 15:12:51 +0300

    testing

--- themes/custom/react_weather/js/src/App.jsx
+++ themes/custom/react_weather/js/src/App.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <h1>Hi</h1>;
+  const [weather, setWeather] = useState("weather");
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .catch((error) => console.log(error))
+      .then((res) => {
+        console.log(res);
+      });
+    setWeather("weather working");
+  }, []);
+
+  return <h1>{weather}</h1>;
 };
 
 export default App;
--- themes/custom/react_weather/webpack.config.js
+++ themes/custom/react_weather/webpack.config.js
@@ -14,6 +14,7 @@ const config = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  // target: "node",
   module: {
     rules: [
       {
